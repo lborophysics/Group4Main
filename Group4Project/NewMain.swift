f43afa8606671013f9bc59190df56401c593d1b6
FredRobs <phfr@lunet.lboro.ac.uk>
2024-02-28 18:18:43 +0000

    One dimension code initial idea

--- Group4Project/NewMain.swift
+++ Group4Project/NewMain.swift
@@ -4,7 +4,3 @@
 //
 //  Created by (s) Freddie Roberts on 28/02/2024.
 //
-
-import Foundation
-
-let newVar = "new"
--- Group4Project/main.swift
+++ Group4Project/main.swift
@@ -24,3 +24,29 @@ print("hello")
 print("TEST")
 
 print("hello there")
+
+import Foundation
+
+let SIZE = 200
+var ez = Array(repeating: 0.0, count: SIZE)
+var hy = Array(repeating: 0.0, count: SIZE)
+let imp0 = 377.0
+let maxTime = 250
+
+for qTime in 0..<maxTime {
+   // Update magnetic field
+   for mm in 0..<(SIZE - 1) {
+       hy[mm] += (ez[mm + 1] - ez[mm]) / imp0
+   }
+
+   // Update electric field
+   for mm in 1..<SIZE {
+       ez[mm] += (hy[mm] - hy[mm - 1]) * imp0
+   }
+
+   // Hardwire a source node
+   ez[0] = exp(-(Double(qTime) - 30.0) * (Double(qTime) - 30.0) / 100.0)
+
+    let fileout = FileWriter(fileName: "Electro.dat")
+    fileout.append_data(data: "\(ez[50])")
+}
