3b2437c6dba8b229d36b8c79520c8b824a5024d7
1JAGs1 <160478597+1JAGs1@users.noreply.github.com>
2024-03-06 16:33:55 +0000

    Update structure.swift

--- Group4Project/structure.swift
+++ Group4Project/structure.swift
@@ -1,1 +1,32 @@
 
+import Foundation
+
+
+public struct EMfield1D{
+    
+    let NumberOfNodes: Double
+    
+    var Ez: [Double]
+    var Hy: [Double]
+    let Impedance: Double
+    
+    let Courant: Double
+    let relativePermitivity: Double
+    let relativePermmeability: Double
+    
+    
+    
+    
+    public init(NumberOfNodes: Double, Ez: [Double], Hy: [Double], Impedance: Double, Courant: Double, relativePermitivity: Double, relativePermmeability: Double){
+        
+        self.NumberOfNodes = NumberOfNodes
+        self.Ez = Ez
+        self.Hy = Hy
+        self.Impedance = Impedance
+        self.Courant = Courant
+        self.relativePermitivity = relativePermitivity
+        self.relativePermmeability = relativePermmeability
+        
+        
+    }
+    
