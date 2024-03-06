2f660e3271bd5f92561c28119619ed2cfb35deb2
That1Cate <C.Georgescu-23@student.lboro.ac.uk>
2024-03-06 16:22:19 +0000

    update on main

--- Group4Project/main.swift
+++ Group4Project/main.swift
@@ -5,25 +5,7 @@
 //  Created by (s) Adrian Chammas on 19/02/2024.
 //
 
-import Foundation
 
-print("Hello, World!")
-
-print("susy")
-
-print("This may be working")
-
-print("Hello")
-
-print("branchie")
-
-print("branchie")
-
-print("hello")
-
-print("TEST")
-
-print("hello there")
 
 import Foundation
 
@@ -33,20 +15,18 @@ var hy = Array(repeating: 0.0, count: SIZE)
 let imp0 = 377.0
 let maxTime = 250
 
+let fileout = FileWriter(fileName: "Electro.dat")
+fileout.write_data(data: "")
+
 for qTime in 0..<maxTime {
    // Update magnetic field
-   for mm in 0..<(SIZE - 1) {
-       hy[mm] += (ez[mm + 1] - ez[mm]) / imp0
-   }
+   update_magnetic(ez: ez, hy: &hy, imp0: imp0)
 
    // Update electric field
-   for mm in 1..<SIZE {
-       ez[mm] += (hy[mm] - hy[mm - 1]) * imp0
-   }
+   update_electric(ez: &ez, hy: hy, imp0: imp0)
 
    // Hardwire a source node
    ez[0] = exp(-(Double(qTime) - 30.0) * (Double(qTime) - 30.0) / 100.0)
 
-    let fileout = FileWriter(fileName: "Electro.dat")
-    fileout.append_data(data: "\(ez[50])")
+    fileout.append_data(data: "\(ez[50])\n")
 }
