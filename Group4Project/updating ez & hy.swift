4ab3432a10638eae6e0593b6e2e9b050d44d2f95
FredRobs <160478540+FredRobs@users.noreply.github.com>
2024-03-18 10:44:38 +0000

    final update ez & hy.swift

--- Group4Project/updating ez & hy.swift
+++ Group4Project/updating ez & hy.swift
@@ -9,8 +9,10 @@ import Foundation
 
 func update_electric(ez: inout[Double], hy: [Double], imp0: Double, qTimeSubtract30: Double) {
     let SIZE = ez.count
+    ez[0] = ez[1]
+    ez[SIZE - 1] = ez[SIZE - 2]
     for mm in 1..<SIZE {
-        ez[mm] += (hy[mm] - hy[mm - 1]) * imp0
+        ez[mm] = ceze[mm] * ez[mm] + cezh[mm] * (hy[mm] - hy[mm - 1])
     }
     ez[hardwireSourceNode] += exp(-(qTimeSubtract30) * (qTimeSubtract30) / 100.0)
 }
@@ -19,7 +21,7 @@ func update_magnetic(ez: [Double], hy: inout[Double], imp0: Double, qTimeSubtrac
     let SIZE = hy.count
     hy[SIZE - 1] = hy[SIZE - 2]
     for mm in 0..<(SIZE - 1) {
-        hy[mm] += (ez[mm + 1] - ez[mm]) / imp0
+        hy[mm] = chyh[mm] * hy[mm] + chye[mm] * (ez[mm + 1] - ez[mm])
     }
     hy[hardwireSourceNode - 1] -= exp(-(qTimeSubtract30) * (qTimeSubtract30) / 100.0) / imp0
 }
