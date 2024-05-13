a3420d432e60fb321a9fa97e8017145c4630beaf
That1Cate <161630310+That1Cate@users.noreply.github.com>
2024-05-13 10:46:23 +0100

    Create SetMagneticElectricCoefficients.swift

---
+++ Group4Project/SetMagneticElectricCoefficients.swift
@@ -0,0 +1,52 @@
+//
+//  SetMagneticElectricCoefficients.swift
+//  Group4Project
+//
+//  Created by (s) Freddie Roberts on 24/04/2024.
+//
+
+import Foundation
+
+func InitialiseElectricField(Size: Int){
+    for mm in 0..<(SIZE){
+        electric_field[mm] = 0.0
+    }
+}
+
+func InitialiseMagneticField(Size: Int){
+    for mm in 0..<(SIZE - 1){
+        magnetic_field[mm] = 0.0
+    }
+}
+
+func SetElectricFieldCoefficient(Size:Int, LOSS_LAYER:Int, impedance:Double,
+                                        relativePermittivity:Double, LOSS:Double){
+    for mm in 0..<(SIZE){
+        if(mm < 100){
+            ceze[mm] = 1.0
+            cezh[mm] = impedance
+        }
+        else if(mm < LOSS_LAYER){
+            ceze[mm] = 1.0
+            cezh[mm] = impedance / relativePermittivity
+        
+        }
+        else{
+            ceze[mm] = (1.0 - LOSS) / (1.0 + LOSS)
+            cezh[mm] = (impedance / relativePermittivity) / (1.0 + LOSS)
+        }
+    }
+}
+
+func SetMagneticFieldCoefficient(Size:Int, LOSS_LAYER:Int, impedance:Double, LOSS:Double){
+    for mm in 0..<(SIZE){
+        if(mm < LOSS_LAYER){
+            chyh[mm] = 1.0
+            chye[mm] = 1.0 / impedance
+        }
+        else{
+            chyh[mm] = (1.0 - LOSS) / (1.0 + LOSS)
+            chye[mm] = (1.0 / impedance) / (1.0 + LOSS)
+        }
+    }
+}
